0ce8975411adbec61fad7db99ba45a028cec9d55
wayne80361 <wayne80361@gmail.com>
2023-10-18 00:59:09 -0400

    GET route for ONE category by its id and include model Product

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -15,9 +15,23 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get("/:id", async (req, res) => {
+  try {
+    // find one category by its `id` value
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post("/", (req, res) => {

1b11361799534d5f6bb71c84eb65fc28dc2516c9
wayne80361 <wayne80361@gmail.com>
2023-10-18 01:24:34 -0400

    DELETE route for ONE category with little twist

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -34,16 +34,39 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
 });
 
 router.put("/:id", (req, res) => {
   // update a category by its `id` value
 });
 
-router.delete("/:id", (req, res) => {
+router.delete("/:id", async (req, res) => {
   // delete a category by its `id` value
+  try {
+    // find it first so I can use dot notataion to retrive its name
+    const category = await Category.findByPk(req.params.id);
+
+    if (!category) {
+      res.status(404).json("No category found with this id!");
+      return;
+    }
+    // destory/delete the found id
+    await category.destroy();
+
+    const responseMessage = ` Category  ${category.category_name}  deleted successfully. `;
+    console.log(responseMessage);
+    res.status(200).json(responseMessage);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
